805b68e83c2c3c4b7c02b19263a33ac4d9812bfb
frodoeric <frodoeric@hotmail.com>
2015-02-20 17:18:01 -0200

    excpeption

---
+++ Projects/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                //throw new Exception("Erro no Error");
+
+                int x = 1;
+                int y = 0;
+
+                Adicionar(0, 0);
+
+
+                int z = x / y;
+
+
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("Arquivo não encontrado");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.Read();
+            }
+
+        }
+
+        static int Adicionar(int x, int y)
+        {
+            if (x <= 0 || y <= 0)
+            {
+                throw new ArgumentException("x é maior que zero ou y é maior que zero");
+
+            }
+            return x + y;
+        }
+
+    }
+
+}
