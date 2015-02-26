ff31abe679ff11e63433b2a1cf23f393f9c022f8
frodoeric <frodoeric@hotmail.com>
2015-02-26 17:53:01 -0300

    c# última dia

---
+++ Projects/AttributesDemo/AttributesDemo/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttributesDemo
+{
+    [DebuggerDisplay("Nome: {name}")]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          var classe = new Teste();
+          var type = classe.GetType(); // pega o tipo da instancia
+
+          var attributes = type.GetCustomAttributes(typeof (DeveloperInfo), false);//pega todos os atributos que são desse tipo
+
+          foreach (var attribute in attributes)
+          {
+              var devInfo = (DeveloperInfo)attribute;
+            Console.WriteLine((devInfo.EmailAddress));
+            Console.WriteLine(devInfo.Revision);
+          }
+
+//#pragma warning disable 0618 // diretiva de compilação para ignorar o warning.
+//          QualquerCoisa();
+//#pragma warning restore 0618
+        }
+
+      //[Obsolete("Ninguém usa mais essa pinóia!")]
+      //public static void QualquerCoisa()
+      //{
+       
+      //}
+
+
+    }
+
+
+    [DeveloperInfo("fulano@fulano.com", 1)]
+  public class Teste
+  {
+    
+  }
+
+    [AttributeUsage(AttributeTargets.All)]//atributo
+  public class DeveloperInfo : Attribute
+  {
+      public string EmailAddress { get; set; }
+      public int Revision { get; set; }
+        
+
+      public DeveloperInfo(string emailAddress, int revision)
+      {
+        EmailAddress = emailAddress;
+        Revision = revision;
+      }
+    
+  }
+
+}
--- Projects/WCFCadastroCliente2/CadastroDeClientes.Client/MainWindow.xaml.cs
+++ Projects/WCFCadastroCliente2/CadastroDeClientes.Client/MainWindow.xaml.cs
@@ -88,6 +88,6 @@ namespace CadastroDeClientes.Client
             dados.AtualizarCliente(clienteSelecionado);
         }
 
-
+        
     }
 }
